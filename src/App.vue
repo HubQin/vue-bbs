1f587c9ebfcd6fbd79c9864f892bf7c2dd8ca643
hubqin <wuhanqinb@gmail.com>
2019-06-02 23:22:39 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,47 @@
-<template lang="pug">
-  <!--挂载点-->
-  div#app
-    img(src="./assets/logo.png")
-    h1 Welcome to my website2!
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <div class="container">
+                <h1>Welcome form bootstrap</h1>
+                <p>This is a test page</p>
+                <p>
+                    <a href="http://www.bootcss.com" class="btn btn-primary btn-lg"
+                    role="button" target="_blank">Learn More <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a>
+                </p>
+            </div>
+        </div>
+        <div class="jumbotrom">
+            <div class="container">
+                <h1>Welcome from Font Awesome</h1>
+                <p>Get vector icons and social logos on your website with Font Awesome, the web’s most popular icon set and toolkit.</p>
+                <p>
+                    <i class="fa fa-camera"></i>
+                    <i class="fa fa-camera fa-2x"></i>
+                    <i class="fa fa-camera fa-3x"></i>
+                    <i class="fa fa-camera fa-4x"></i>
+                    <i class="fa fa-camera fa-5x"></i>
+                    <i class="fa fa-spinner fa-spin"></i>
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-  //引入组件
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app', //组件名称
-  components: {  //包含的子组件
-    HelloWorld
-  }
 }
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 200px;
+    $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+    $fa-font-path: "~font-awesome/fonts/";
+    @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+    @import "~font-awesome/scss/font-awesome";
 
-        h1 {
-            color: #42b983;
-        }
+    body > .container {
+        margin-top: 15px;
+
     }
 </style>
